d06a3eb78aa0b74441c7f3b160258f3748cb7329
Chakib Cerny <ccerny@rollins.edu>
2021-05-03 01:04:26 -0400

    Update notify.py

--- client/notify.py
+++ client/notify.py
@@ -9,7 +9,7 @@ smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 sender_email = "testchakibcerny@gmail.com"
 receiver_email = "chakib.cerny@gmail.com"
-password = 'Ineed4nA'
+password = '########'
 if (args[1] == 'down'):
     message = "Server is down, do not move files to folder"
 if (args[1] == 'up'):
