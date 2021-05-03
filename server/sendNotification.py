b6208996f6e590a8611de2ab117537bd19608fa3
Chakib Cerny <ccerny@rollins.edu>
2021-05-03 01:03:08 -0400

    Update sendNotification.py

--- server/sendNotification.py
+++ server/sendNotification.py
@@ -1,6 +1,6 @@
 import smtplib
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("testchakibcerny@gmail.com", "Ineed4nA")
+server.login("testchakibcerny@gmail.com", "########")
 server.sendmail("testchakibcerny@gmail.com", "chakib.cerny@gmail.com", "Memory alert ! empty owncloud !")
 server.quit()
